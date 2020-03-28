c591c0d5772229748158a6767895c231bed659f6
Marcel H <marcel.holzhaeuser@freenet.de>
2020-03-28 14:15:40 +0100

    Updatet Camera Movement

---
+++ SSJ20_CoVide_Project/Assets/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour
+{
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.tag != "Player")
+            GameObject.Destroy(collision.gameObject);
+    }
+}
---
+++ SSJ20_CoVide_Project/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject[] pickups;
+    public GameObject[] obstacles;
+    public GameObject[] road;
+    public GameObject[] houses;
+
+    private float obstacleTimer;
+    public float minObstacleTime = 1;
+    public float maxObstacleTime = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obstacleTimer = Random.Range(minObstacleTime, maxObstacleTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (obstacleTimer < 0)
+        {
+
+            obstacleTimer = Random.Range(minObstacleTime,maxObstacleTime);
+        }
+    }
+}
