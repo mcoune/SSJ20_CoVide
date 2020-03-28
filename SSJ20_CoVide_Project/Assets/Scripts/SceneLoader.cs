35c8aaf3682adc00458198f70123b9cb3cf8dc4c
LordGM <meissner.olaf@gmail.com>
2020-03-28 23:43:41 +0100

    Added Win Scene Added Game Over Scene Added Hot to Play Scene Added scenes to Build Settings

---
+++ SSJ20_CoVide_Project/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadScene(int Scene)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
