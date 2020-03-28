b76b97890971ff4a029a49535a43e83d7a39b3e5
Marcel H <marcel.holzhaeuser@freenet.de>
2020-03-28 10:41:51 +0100

    Created Basic Movement

---
+++ SSJ20_CoVide_Project/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public KeyCode up;
+    public KeyCode left;
+    public KeyCode down;
+    public KeyCode right;
+
+    public float upSpeed = 1f;
+    public float downSpeed = 1f;
+    public float horizontalSpeed = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(up))
+        {
+            transform.position += Vector3.up * upSpeed;
+        }
+        if (Input.GetKey(left))
+        {
+            transform.position += Vector3.left * horizontalSpeed;
+        }
+        if (Input.GetKey(down))
+        {
+            transform.position += Vector3.down * downSpeed;
+        }
+        if (Input.GetKey(right))
+        {
+            transform.position += Vector3.right * horizontalSpeed;
+        }
+    }
+}
