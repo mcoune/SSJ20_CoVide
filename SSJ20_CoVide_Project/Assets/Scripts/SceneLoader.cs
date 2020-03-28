56fe9e42e65508a81c17ba39e1a61b17cad901dd
LordGM <meissner.olaf@gmail.com>
2020-03-28 23:56:24 +0100

    Quitting the game using the Quit Buttons now resets the player scores.

--- SSJ20_CoVide_Project/Assets/Scripts/SceneLoader.cs
+++ SSJ20_CoVide_Project/Assets/Scripts/SceneLoader.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class SceneLoader : MonoBehaviour
 {
+    public ScoreObject scoreObject1;
+    public ScoreObject scoreObject2;
+
 
     public void LoadScene(int Scene)
     {
@@ -13,6 +16,11 @@ public class SceneLoader : MonoBehaviour
 
     public void Quit()
     {
+        scoreObject1.score = 0;
+        scoreObject1.deliveries = 0;
+
+        scoreObject2.score = 0;
+        scoreObject2.deliveries = 0;
         Application.Quit();
     }
 }
