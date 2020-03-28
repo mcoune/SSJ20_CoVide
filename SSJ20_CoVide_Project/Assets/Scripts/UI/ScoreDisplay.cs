7969fe8eb5c9e5899f960455dd031ca552ad95d0
Michael Couné <michael.coune@gmx.de>
2020-03-28 20:54:39 +0100

    added score logic to score ui

---
+++ SSJ20_CoVide_Project/Assets/Scripts/UI/ScoreDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    public ScoreObject score;
+    public Text scoreText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = score.score.ToString();
+    }
+}
