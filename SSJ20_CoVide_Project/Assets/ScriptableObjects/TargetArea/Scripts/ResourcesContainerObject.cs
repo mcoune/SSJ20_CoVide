764114b1234b4e5f1dc3e19efe511ddf7abfb00d
Michael Couné <michael.coune@gmx.de>
2020-03-29 10:58:18 +0200

    update targetArea

---
+++ SSJ20_CoVide_Project/Assets/ScriptableObjects/TargetArea/Scripts/ResourcesContainerObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The <see cref="ResourcesContainerObject"/> class.
+/// </summary>
+[CreateAssetMenu(fileName = "ResourceContainer", menuName = "Resources/Container")]
+public class ResourcesContainerObject : ScriptableObject
+{
+    public List<ItemObject> resourceContainer;
+}
