0e1db5f3f30e4a44eb29fbebe6d7adc8bf9b3328
Michael Couné <michael.coune@gmx.de>
2020-03-29 16:08:56 +0200

    disable and enaled targetareas

--- SSJ20_CoVide_Project/Assets/Scripts/Target/TargetArea.cs
+++ SSJ20_CoVide_Project/Assets/Scripts/Target/TargetArea.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +8,10 @@ public class TargetArea : MonoBehaviour
 
     [HideInInspector]
     public ItemObject requestResource;
+
+    [HideInInspector]
+    public bool scoreIsEnabled = false;
+
     public SpriteRenderer itemRenderer;
 
     private bool resourceDelivered = false;
@@ -35,8 +39,11 @@ public class TargetArea : MonoBehaviour
         {
             if (throwable.item == requestResource)
             {
-                scoreController.AddDelivery(1);
-                scoreController.AddScore(requestResource.points + ScoreMultiplier);
+                if(scoreIsEnabled)
+                {
+                    scoreController.AddDelivery(1);
+                    scoreController.AddScore(requestResource.points + ScoreMultiplier);
+                }
             }
             else
             {
@@ -44,8 +51,16 @@ public class TargetArea : MonoBehaviour
             }
         }
 
+        Transform parent = transform.parent;
+        var targetAreas = parent.GetComponentsInChildren<TargetArea>().ToList();
+        targetAreas.ForEach(x => x.enabled = false);
         itemRenderer.enabled = false;
         resourceDelivered = true;
         Destroy(other.gameObject);
+    }
+
+    public void Showitem()
+    {
+        itemRenderer.enabled = scoreIsEnabled;
     }
 }
--- SSJ20_CoVide_Project/Assets/Scripts/Target/TargetController.cs
+++ SSJ20_CoVide_Project/Assets/Scripts/Target/TargetController.cs
@@ -8,21 +8,44 @@ public class TargetController : MonoBehaviour
     public ResourcesContainerObject resourcesContainer;
     public SpriteRenderer itemRenderer;
 
+    private List<TargetArea> targetAreas;
+    private ItemObject requestedResource;
+
     // Start is called before the first frame update
     void Awake()
     {
         System.Random r = new System.Random();
-        var requestResource = resourcesContainer.resourceContainer[r.Next(resourcesContainer.resourceContainer.Count)];
+        requestedResource = resourcesContainer.resourceContainer[r.Next(resourcesContainer.resourceContainer.Count)];
 
-        var childs = transform.GetComponentsInChildren<TargetArea>().ToList();
-        childs.ForEach(x =>
-        {
-            x.requestResource = requestResource;
-            x.itemRenderer = itemRenderer;
-        });
+        targetAreas = transform.GetComponentsInChildren<TargetArea>().ToList();
+    }    
 
-        var collectablePrefab = requestResource.collectablePrefab;
+    /// <summary>
+    /// Sets the requested resource
+    /// </summary>
+    private void SetResource()
+    {
+        targetAreas.ForEach(x => x.requestResource = requestedResource);
+    }
+
+    /// <summary>
+    /// Sets the item renderer and allocates sprite
+    /// </summary>
+    private void SetItemRenderer()
+    {
+        targetAreas.ForEach(x => x.itemRenderer = itemRenderer);
+        var collectablePrefab = requestedResource.collectablePrefab;
         var prefabRenderer = collectablePrefab.GetComponent<SpriteRenderer>();
         itemRenderer.sprite = prefabRenderer.sprite;
+
+    }
+
+    /// <summary>
+    /// Enables the score for all target areas
+    /// </summary>
+    /// <param name="isEnable"></param>
+    public void EnableScoreing(bool isEnable)
+    {
+        targetAreas.ForEach(x => x.scoreIsEnabled = isEnable);
     }
 }
