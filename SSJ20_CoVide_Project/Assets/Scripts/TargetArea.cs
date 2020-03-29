f10e4981ce0792b2cabffcf645b7af565197a1c4
Michael Couné <michael.coune@gmx.de>
2020-03-29 12:50:45 +0200

    targetarea reworked

--- SSJ20_CoVide_Project/Assets/Scripts/TargetArea.cs
+++ SSJ20_CoVide_Project/Assets/Scripts/TargetArea.cs
@@ -4,31 +4,43 @@ using UnityEngine;
 
 public class TargetArea : MonoBehaviour
 {
-    public ItemObject requestResource;
-    
+    public ResourcesContainerObject resourcesContainer;
+    public int ScoreMultiplier;
+    private ItemObject requestResource;
+
+    public void Awake()
+    {
+        System.Random r = new System.Random();
+        requestResource = resourcesContainer.resourceContainer[r.Next(resourcesContainer.resourceContainer.Count)];
+    }
+
     public void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.tag != "Projectile")
+        {
+            return;
+        }
+
         var throwable = other.GetComponent<Throwable>();
-        if(throwable == null || throwable.owner == null)
-        {           
+        if (throwable == null || throwable.owner == null)
+        {
             return;
         }
 
         var scoreController = throwable.owner.GetComponent<ScoreController>();
-        if(scoreController ==null)
-        {            
+        if (scoreController == null)
+        {
             return;
         }
-                
-        if(throwable.item == requestResource)
+
+        if (throwable.item == requestResource)
         {
-            FindObjectOfType<AudioManager>().Play("NpcCatch");
             scoreController.AddDelivery(1);
-            scoreController.AddScore(requestResource.points);
+            scoreController.AddScore(requestResource.points + ScoreMultiplier);
         }
         else
         {
-            scoreController.AddScore(-requestResource.penalty);
+            scoreController.AddScore(-requestResource.penaltiy);
         }
 
 
